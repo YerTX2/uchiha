3a2ad6e54824fb1f73ef6910f27a9544ba8745da
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-01 03:22:56 -0500

    Actualizar menu-help.js

--- plugins/menu-help.js
+++ plugins/menu-help.js
@@ -138,10 +138,19 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 
     
 
-let gifUrl = "https://tinyurl.com/2bsjy5mu";
-//await conn.sendMessage(m.chat, {video: {url: gifUrl}, gifPlayback: true, caption: text.trim(), mentions: [m.sender]}, {quoted: m})
+let vid = "https://i.ibb.co/0Q43DP8/file.jpg";
+    let gifUrl = "https://tinyurl.com/2bsjy5mu";
+    let img2 = "https://tinyurl.com/22qr2yxq";
+    let img3 = "https://tinyurl.com/22qr2yxq";
+    let img4 = "https://tinyurl.com/22qr2yxq";
+    let img5 = "https://tinyurl.com/22qr2yxq";
+    let img6 = "https://tinyurl.com/22qr2yxq";
+    let img8 = "https://tinyurl.com/22qr2yxq";
+    let img9 = "https://tinyurl.com/22qr2yxq";
+    let img10 = "https://tinyurl.com/22qr2yxq";
+    let img11 = "https://tinyurl.com/22qr2yxq";
 
-    await conn.sendListB(m.chat, menu, txt, `「✶」𝐌𝐄𝐍𝐔 𝐋𝐈𝐒𝐓「✶」`, [img, img2, img3, img4, img5, img6, img8, img9, img10, img11].getRandom(), listSections, estilo);
+    await conn.sendListB(m.chat, menu, txt, `「✶」𝐌𝐄𝐍𝐔 𝐋𝐈𝐒𝐓「✶」`, [vid,  gifUrl, img2, img3, img4, img5, img6, img8, img9, img10, img11].getRandom(), listSections, estilo);
 };
 
 
