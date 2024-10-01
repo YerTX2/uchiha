3d102b3458b69fbbd4088d06c5b996240f0f906e
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-01 03:54:00 -0500

    Actualizar menu-help.js

--- plugins/menu-help.js
+++ plugins/menu-help.js
@@ -61,5 +61,15 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     txt += '`ᴀᴄᴛɪᴠᴏ ::`' + ` ${uptime}\n`;
     txt += '`ᴄᴏɴᴛᴀᴄᴛᴏ ::` #owner\n\n';
     txt += "> Yer🇦🇱⚡";
+let img = await (await fetch(`https://qu.ax/mtM.jpg`)).buffer()
 
+   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
+
+  } catch (e) {
+    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
+    throw e
+  }
+}
    
